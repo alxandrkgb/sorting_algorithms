36241a323fcc13b9c20aeb0ed8f85c7feeb23c69
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-06-27 07:50:16 -0700

    Initial commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,34 +6,34 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current;
+	listint_t *current;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    current = (*list)->next;
-    while (current != NULL)
-    {
-        listint_t *next = current->next;
-        listint_t *temp = current;
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		listint_t *next = current->next;
+		listint_t *temp = current;
 
-        while (temp->prev != NULL && temp->n < temp->prev->n)
-        {
-            if (temp->next != NULL)
-                temp->next->prev = temp->prev;
-            temp->prev->next = temp->next;
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
 
-            if (temp->prev == NULL)
-                *list = temp;
-            else
-                temp->prev->next = temp;
+			if (temp->prev == NULL)
+				*list = temp;
+			else
+				temp->prev->next = temp;
 
-            print_list(*list);
-        }
+			print_list(*list);
+		}
 
-        current = next;
-    }
+		current = next;
+	}
 }
