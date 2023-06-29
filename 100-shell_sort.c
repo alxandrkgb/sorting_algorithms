60ecce084fde1db6209358c7cd5bdc5a859b80a9
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-06-29 14:39:50 +0000

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,40 +1,41 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * shell_sort - Sorts an array of integers in ascending order using Shell sort
- *
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
+*swap - the positions of two elements into an array
+*@array: array
+*@item1: array element
+*@item2: array element
+*/
+void swap(int *array, int item1, int item2)
+{
+
+	int tmp;
+
+	tmp = array[item1];
+	array[item1] = array[item2];
+	array[item2] = tmp;
+}
+/**
+ * shell_sort - function that sorts an array of integers in ascending
+ * order using the Shell sort algorithm, using the Knuth sequence
+ * @size: size of the array
+ * @array: list with numbers
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1;
-    size_t i, j;
-    int temp;
+	size_t gap = 1, i, index = 0;
 
-    if (array == NULL || size < 2)
-        return;
-
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
-
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            temp = array[i];
-
-            for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-            {
-                array[j] = array[j - gap];
-            }
-
-            array[j] = temp;
-        }
-
-        gap = (gap - 1) / 3;
-
-        print_array(array, size);
-    }
+	if (array == NULL || size < 2)
+		return;
+	while (gap < size / 3)
+		gap = 3 * gap + 1;
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+			for (index = i; index >= gap &&
+			 (array[index] < array[index - gap]); index -= gap)
+				swap(array, index, index - gap);
+		print_array(array, size);
+		gap /= 3;
+	}
 }
