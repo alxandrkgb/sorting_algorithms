9bdb66adbfa9ae9e0fc3c167261e0f7fd6d0113e
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-06-29 14:53:40 +0000

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,51 +1,114 @@
-#include <stdlib.h>
 #include "deck.h"
+#include <stdio.h>
+/**
+ *_strcmp - compare two strings
+ *@str1: string
+ *@str2: string
+ *Return: 1 str1 and str2 is equal, 0 they are not equal
+ */
+int _strcmp(const char *str1, char *str2)
+{
+	size_t i = 0;
 
-int card_value(const card_t *card);
+	if (str1 == '\0')
+		return (0);
+	while (str1[i])
+	{
+		if (str1[i] != str2[i])
+			return (0);
+		i++;
+	}
+	if (str1[i] == '\0' && str2[i])
+		return (0);
+	return (1);
+}
+/**
+ * get_card_position - return the position based on card you put in
+ * @node: represent the card
+ * Return: return the card position
+ */
+int get_card_position(deck_node_t *node)
+{
+	int value;
+
+	value = (*node).card->value[0] - '0';
+	if (value < 50 || value > 57)
+	{
+		if (_strcmp((*node).card->value, "Ace") == 1)
+			value = 1;
+		else if (_strcmp((*node).card->value, "10") == 1)
+			value = 10;
+		else if (_strcmp((*node).card->value, "Jack") == 1)
+			value = 11;
+		else if (_strcmp((*node).card->value, "Queen") == 1)
+			value = 12;
+		else if (_strcmp((*node).card->value, "King") == 1)
+			value = 13;
+	}
+	value += (*node).card->kind * 13;
+	return (value);
+}
+/**
+ *swap_card - swap a card for his previous one
+ *@card: card
+ *@deck: card deck
+ *Return: return a pointer to a card which was enter it
+ */
+deck_node_t *swap_card(deck_node_t *card, deck_node_t **deck)
+{
+	deck_node_t *back = card->prev, *current = card;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*deck = current;
+	return (current);
+}
 
 /**
- * sort_deck - Sorts a deck of cards in ascending order
- * @deck: The deck of cards to be sorted
+ * insertion_sort_deck - function that sorts a doubly linked deck
+ * of integers in ascending order using the Insertion sort algorithm
+ * @deck: Dobule linked deck to sort
+ */
+void insertion_sort_deck(deck_node_t **deck)
+{
+	int value_prev, value_current;
+	deck_node_t *node;
+
+	if (deck == NULL || (*deck)->next == NULL)
+		return;
+	node = (*deck)->next;
+	while (node)
+	{
+		/* preparing the previous value */
+		if (node->prev)
+		{
+			value_prev = get_card_position((node->prev));
+			value_current = get_card_position(node);
+		}
+		while ((node->prev) && (value_prev > value_current))
+		{
+			value_prev = get_card_position((node->prev));
+			value_current = get_card_position(node);
+			node = swap_card(node, deck);
+
+		}
+		node = node->next;
+	}
+}
+/**
+ * sort_deck - sort a deck you put in using
+ * insertion sort algorithm
+ * @deck: deck
  */
 void sort_deck(deck_node_t **deck)
 {
-    deck_node_t *node;
-    card_t **cards;
-    size_t deck_size = 0;
-    size_t i, j;
-
-    /* Count the number of cards in the deck */
-    for (node = *deck; node != NULL; node = node->next)
-        deck_size++;
-
-    /* Allocate memory for an array of card pointers */
-    cards = malloc(deck_size * sizeof(card_t *));
-    if (cards == NULL)
-        return;
-
-    /* Copy the card pointers to the array */
-    for (i = 0, node = *deck; node != NULL; i++, node = node->next)
-        cards[i] = (card_t *)node->card;
-
-    /* Perform bubble sort on the card pointers */
-    for (i = 0; i < deck_size - 1; i++)
-    {
-        for (j = 0; j < deck_size - i - 1; j++)
-        {
-            if (card_value(cards[j]) > card_value(cards[j + 1]))
-            {
-                /* Swap the card pointers */
-                card_t *temp = cards[j];
-                cards[j] = cards[j + 1];
-                cards[j + 1] = temp;
-            }
-        }
-    }
-
-    /* Update the deck with the sorted cards */
-    for (i = 0, node = *deck; node != NULL; i++, node = node->next)
-        node->card = (void *)cards[i];
-
-    /* Free the allocated memory */
-    free(cards);
+	insertion_sort_deck(deck);
 }
